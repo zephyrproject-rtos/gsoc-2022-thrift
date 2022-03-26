cea544a79bcd61ec827f1e49af0bd4c2bb61ad25
Christopher Friedt <chrisfriedt@gmail.com>
2022-03-26 09:39:55 -0400

    tests + samples: switch from west build to twister
    Some additional work was needed to get tests to build / run with
    twister.
    
    Fixes #67
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- lib/thrift/include/sys/poll.h
+++ lib/thrift/include/sys/poll.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef CONFIG_ARCH_POSIX
+
+#include_next <sys/poll.h>
+
+#else
+
 /*
  * Thrift mistakenly includes <sys/poll.h> instead of <poll.h>
  * See https://pubs.opengroup.org/onlinepubs/9699919799/functions/poll.html
@@ -21,3 +27,5 @@ struct linger
     int l_onoff;  /* option on/off */
     int l_linger; /* linger time */
 };
+
+#endif
