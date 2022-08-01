725e97e3db660f5d9558f4e96f41c86269201940
SdtElectronics <meiyang423@Gmail.com>
2022-08-01 22:09:19 +0800

    TZlibTransport: import Inflater
    This commit adds a 3rd party decompression library - Inflater as the
    implementation of the DEFLATE decompression algorithm.
    
    Origin: Inflater
    License: MIT
    URL: https://github.com/martin-rizzo/Inflater
    Commit: 3c9669f4ff07995d13ac50ed41fd9346be08f33b
    Purpose: DEFLATE decompression algorithm support for thrift
    
    Signed-off-by: Yang Mei <meiyang423@gmail.com>

---
+++ lib/thrift/muzic/Inflater/extra/examples/uzo-feed/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  uzo-feed
+//
+//  Created by Martin on 23/06/2020.
+//  Copyright © 2020 Martin Rizzo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
