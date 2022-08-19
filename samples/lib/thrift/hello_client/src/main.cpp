96bd64407cb49c5dbddc8a38130cb65d619a6fd8
SdtElectronics <meiyang423@Gmail.com>
2022-08-19 16:57:55 +0800

    samples: adopt new structure to cover all features
    * Adopt new structure which supports configurable combination of features
    * Cover compactProtocol, zlibTransport and tlsTransport
    
    Signed-off-by: Yang Mei <meiyang423@gmail.com>

--- samples/lib/thrift/hello_client/src/main.cpp
+++ samples/lib/thrift/hello_client/src/main.cpp
@@ -14,15 +14,33 @@
 #include <fcntl.h>
 
 #include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/protocol/TCompactProtocol.h>
 #include <thrift/transport/TBufferTransports.h>
-#include <thrift/transport/TFDTransport.h>
+#include <thrift/transport/TSSLSocket.h>
 #include <thrift/transport/TSocket.h>
+#include <thrift/transport/TZlibTransport.h>
 
 #include "Hello.h"
 
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
+
+#ifndef IS_ENABLED
+#define IS_ENABLED(flag) flag
+#endif
+
+#ifndef CONFIG_THRIFT_COMPACT_PROTOCOL
+#define CONFIG_THRIFT_COMPACT_PROTOCOL 0
+#endif
+
+#ifndef CONFIG_THRIFT_ZLIB_TRANSPORT
+#define CONFIG_THRIFT_ZLIB_TRANSPORT 0
+#endif
+
+#ifndef CONFIG_THRIFT_SSL_SOCKET
+#define CONFIG_THRIFT_SSL_SOCKET 0
+#endif
 
 #ifdef __ZEPHYR__
 int main(void)
@@ -43,9 +61,49 @@ int main(int argc, char** argv)
 #endif
 
   int port = 4242;
-  std::shared_ptr<TTransport> trans(new TSocket(my_addr, port));
-  std::shared_ptr<TTransport> transport(new TBufferedTransport(trans));
-  std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  std::shared_ptr<TSSLSocketFactory> socketFactory;
+  std::shared_ptr<TTransport> trans;
+  if (IS_ENABLED(CONFIG_THRIFT_SSL_SOCKET)) {
+    const int port = 4242;
+    socketFactory = std::make_shared<TSSLSocketFactory>();
+    socketFactory->authenticate(true);
+#ifdef __ZEPHYR__
+    socketFactory->loadCertificateFromBuffer(
+#include "../../hello_common/qemu-cert.pem"
+    );
+    socketFactory->loadPrivateKeyFromBuffer(
+#include "../../hello_common/qemu-key.pem"
+    );
+    socketFactory->loadTrustedCertificatesFromBuffer(
+#include "../../hello_common/native-cert.pem"
+    );
+#else
+    socketFactory->loadCertificateFromBuffer(
+#include "../../hello_common/native-cert.pem"
+    );
+    socketFactory->loadPrivateKeyFromBuffer(
+#include "../../hello_common/native-key.pem"
+    );
+    socketFactory->loadTrustedCertificatesFromBuffer(
+#include "../../hello_common/qemu-cert.pem"
+    );
+#endif
+    trans = socketFactory->createSocket(my_addr, port);
+  } else {
+    trans = std::make_shared<TSocket>(my_addr, port);
+  }
+  std::shared_ptr<TTransport> transport;
+  if (IS_ENABLED(CONFIG_THRIFT_ZLIB_TRANSPORT)) {
+    transport = std::make_shared<TZlibTransport>(trans);
+  } else {
+    transport = std::make_shared<TBufferedTransport>(trans);
+  }
+  std::shared_ptr<TProtocol> protocol;
+  if (IS_ENABLED(CONFIG_THRIFT_COMPACT_PROTOCOL)) {
+    protocol = std::make_shared<TCompactProtocol>(transport);
+  } else {
+    protocol = std::make_shared<TBinaryProtocol>(transport);
+  }
   HelloClient client(protocol);
 
   try {
