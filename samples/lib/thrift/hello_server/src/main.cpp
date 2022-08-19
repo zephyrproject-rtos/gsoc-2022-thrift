cd1ddd015a90fd83b0cc097ccffff665a4486bfe
SdtElectronics <meiyang423@Gmail.com>
2022-08-19 16:57:55 +0800

    samples: adopt new structure to cover all features
    * Adopt new structure which supports configurable combination of features
    * Cover compactProtocol, zlibTransport and tlsTransport
    
    Signed-off-by: Yang Mei <meiyang423@gmail.com>

--- samples/lib/thrift/hello_server/src/main.cpp
+++ samples/lib/thrift/hello_server/src/main.cpp
@@ -12,9 +12,13 @@
 #include <cstdlib>
 
 #include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/protocol/TCompactProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/transport/TSSLServerSocket.h>
+#include <thrift/transport/TSSLSocket.h>
 #include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TZlibTransport.h>
 
 #include "Hello.h"
 #include "HelloHandler.h"
@@ -23,6 +27,22 @@ using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
+
+#ifndef IS_ENABLED
+#define IS_ENABLED(flag) flag
+#endif
+
+#ifndef CONFIG_THRIFT_COMPACT_PROTOCOL
+#define CONFIG_THRIFT_COMPACT_PROTOCOL 0
+#endif
+
+#ifndef CONFIG_THRIFT_ZLIB_TRANSPORT
+#define CONFIG_THRIFT_ZLIB_TRANSPORT 0
+#endif
+
+#ifndef CONFIG_THRIFT_SSL_SOCKET
+#define CONFIG_THRIFT_SSL_SOCKET 0
+#endif
 
 #ifdef __ZEPHYR__
 int main(void)
@@ -46,9 +66,48 @@ int main(int argc, char** argv)
   int port = 4242;
   std::shared_ptr<HelloHandler> handler(new HelloHandler());
   std::shared_ptr<TProcessor> processor(new HelloProcessor(handler));
-  std::shared_ptr<TServerTransport> serverTransport(new TServerSocket(my_addr, port));
-  std::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  std::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+  std::shared_ptr<TServerTransport> serverTransport;
+  if (IS_ENABLED(CONFIG_THRIFT_SSL_SOCKET)) {
+    const int port = 4242;
+    std::shared_ptr<TSSLSocketFactory> socketFactory(new TSSLSocketFactory());
+    socketFactory->server(true);
+#ifdef __ZEPHYR__
+    socketFactory->loadCertificateFromBuffer(
+#include "../../hello_common/qemu-cert.pem"
+    );
+    socketFactory->loadPrivateKeyFromBuffer(
+#include "../../hello_common/qemu-key.pem"
+    );
+    socketFactory->loadTrustedCertificatesFromBuffer(
+#include "../../hello_common/native-cert.pem"
+    );
+#else
+    socketFactory->loadCertificateFromBuffer(
+#include "../../hello_common/native-cert.pem"
+    );
+    socketFactory->loadPrivateKeyFromBuffer(
+#include "../../hello_common/native-key.pem"
+    );
+    socketFactory->loadTrustedCertificatesFromBuffer(
+#include "../../hello_common/qemu-cert.pem"
+    );
+#endif
+    serverTransport = std::make_shared<TSSLServerSocket>("0.0.0.0", port, socketFactory);
+  } else {
+    serverTransport = std::make_shared<TServerSocket>(my_addr, port);
+  }
+  std::shared_ptr<TTransportFactory> transportFactory;
+  if (IS_ENABLED(CONFIG_THRIFT_ZLIB_TRANSPORT)) {
+    transportFactory = std::make_shared<TZlibTransportFactory>();
+  } else {
+    transportFactory = std::make_shared<TBufferedTransportFactory>();
+  }
+  std::shared_ptr<TProtocolFactory> protocolFactory;
+  if (IS_ENABLED(CONFIG_THRIFT_COMPACT_PROTOCOL)) {
+    protocolFactory = std::make_shared<TCompactProtocolFactory>();
+  } else {
+    protocolFactory = std::make_shared<TBinaryProtocolFactory>();
+  }
   TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
 
   try {
